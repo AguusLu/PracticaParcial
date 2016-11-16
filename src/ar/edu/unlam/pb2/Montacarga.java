26b1618c2bb65b78bcdb7652c3a472348cdbf140
AguusLu <agus.luu@gmail.com>
2016-11-16 12:25:40 -0300

    commit

---
+++ src/ar/edu/unlam/pb2/Montacarga.java
@@ -0,0 +1,52 @@
+package ar.edu.unlam.pb2;
+
+import java.util.Collection;
+import java.util.TreeSet;
+
+public class Montacarga {
+	
+	private TreeSet<Carga> cargas; 
+	private Integer sumatoria;
+	
+	public Integer getSumatoria() {
+		return sumatoria;
+	}
+
+	public void setSumatoria(Integer sumatoria) {
+		this.sumatoria = sumatoria;
+	}
+
+	public void sumatoriaDeCargas(Carga cargas)
+	{
+		for(int i=0; i<obtenerCantidadDeCargas();i++)
+		{	
+	//		suma = suma + cargas.
+		}
+	}
+	
+	public Integer obtenerCantidadDeCargas()
+	{
+		return this.cargas.size();
+	}
+	
+	public void ingresarCarga(Carga cargas) throws Exception
+	{
+		if(!this.cargas.add(cargas));
+		{
+			{
+				throw new Exception("No se admiten" + cargas.getPeso() );
+			}
+		}
+	}
+
+	
+	
+	public TreeSet<Carga> getCargas() {
+		return cargas;
+	}
+
+	public void setCargas(TreeSet<Carga> cargas) {
+		this.cargas = cargas;
+	}
+
+}
